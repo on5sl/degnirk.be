22c078bba174e05c189063d670e10b1ff67e4794
on5sl <bramverswalm@gmail.com>
2013-12-22 16:21:19 +0100

    Added box shadow

--- degnirk.be/degnirk.be/scripts/DeGnirk.js
+++ degnirk.be/degnirk.be/scripts/DeGnirk.js
@@ -6,31 +6,40 @@ var backgroundCssTop;
 $(window).on("backstretch.after", function (e, instance, index) {
     //De code in de if wordt enkel uitgevoerd na de eerste afbeelding, zodat een slideshow later geen problemen geeft
     if (index === 0) {
+        $('header').css({
+            "box-shadow": "inset 0 -10px 20px 0 #000"
+        });
         //TODO: Dit kan beter vervangen door een reeks animaties zodat de tekst pas erin komt nadat het logo centraal staat
         $('#logoHolder').animate({ top: "0", opacity: "1" }, { duration: 1000 });
-        $('nav').fadeIn(2000);
+        $('#subnav').fadeIn(2000);
         backgroundCssTop = parseFloat(instance.$img.css('top'), 10);
     }
 });
 
 $(document).ready(function () {
-
     $('header').height($(window).height());
 
     $(window).resize(function () {
         $('header').height($(window).innerHeight);
     });
 
-    
     $(window).scroll(function () {
         $('header').backstretch("pause");
-        var coords = backgroundCssTop + ($(window).scrollTop() / 2) + 'px';
+        var scrollTop = $(window).scrollTop();
+        var coords = backgroundCssTop + (scrollTop / 2) + 'px';
         $('header img').css({
             top: coords
-    });
+        });
+
+        if (scrollTop >= $('#content').offset().top - ($('#navbar').height())) {
+            $('#navbar').show();
+        } else {
+            $('#navbar').hide();
+        }
+
     });
 
-    $('body').niceScroll({ mousescrollstep: 10, cursorborder: '0px', cursorwidth: '7px', cursoropacitymax: 0.5 });
+    $('body').niceScroll({ mousescrollstep: 20, cursorborder: '0px', cursorwidth: '7px', cursoropacitymax: 0.5 });
 
     var angle1 = 0;
     setInterval(function () {
@@ -43,8 +52,6 @@ $(document).ready(function () {
         angle2 -= 0.3;
         $("#degnirkouterring").rotate(angle2);
     }, 70);
-
-
 
 
     //https://api.foursquare.com/v2/venues/4d7be2a0f260a093e61f30ba/herenow?client_id=3UFSGCLAXPLGASVZIWDYEF3NL24SDC1RYYS1DMKDTGFJQJ1L&client_secret=XK4WTKZHT2HTUP3OTBCX1G55IST1X2ZQHQNG1GDVYBB2KHWH&v=20130827

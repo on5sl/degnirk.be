a30fc641af297389bfe7370bb74bc369238526db
on5sl <bramverswalm@gmail.com>
2013-12-28 17:47:53 +0100

    Added some templates, added facebook albums, seperated the home javascript, refactored ToExpando

--- degnirk.be/degnirk.be/Models/FacebookEventsModel.cs
+++ degnirk.be/degnirk.be/Models/FacebookEventsModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Web;
 using System.Web.Mvc;
+using degnirk.be.Helpers;
 using Facebook;
 
 namespace degnirk.be.Models
@@ -43,23 +44,8 @@ namespace degnirk.be.Models
                 link = EventUri + fbevent.eid,
                 attendingCount = fbevent.attending_count,
                 picCover = fbevent.pic_cover.source,
-                start_time = fbevent.start_time
+                startTime = fbevent.start_time
             }.ToExpando());
-
-            
         }
-    }
-
-    public static class ExpandoHelper
-    {
-        public static ExpandoObject ToExpando(this object anonymousObject)
-        {
-            IDictionary<string, object> anonymousDictionary = HtmlHelper.AnonymousObjectToHtmlAttributes(anonymousObject);
-            IDictionary<string, object> expando = new ExpandoObject();
-            foreach (var item in anonymousDictionary)
-                expando.Add(item);
-            return (ExpandoObject)expando;
-        }
-
     }
 }
--- degnirk.be/degnirk.be/scripts/DeGnirk.js
+++ degnirk.be/degnirk.be/scripts/DeGnirk.js
@@ -1,73 +1,10 @@
-﻿//Na het inladen van de html en css beginnen we met de javascript te verwerken
-//We laden de afbeeldingen in op het header element
-$('header').backstretch(images, { fade: 'slow', duration: '10000' });
-var backgroundCssTop;
-//Nadat de afbeelding is ingeladen wordt deze functie opgeroepen
-$(window).on("backstretch.after", function (e, instance, index) {
-    //De code in de if wordt enkel uitgevoerd na de eerste afbeelding, zodat een slideshow later geen problemen geeft
-    if (index === 0) {
-        $('header').css({
-            "box-shadow": "inset 0 -10px 20px 0 #000"
-        });
-        //TODO: Dit kan beter vervangen door een reeks animaties zodat de tekst pas erin komt nadat het logo centraal staat
-        $('#logoHolder').animate({ top: "0", opacity: "1" }, { duration: 1000 });
-        $('#subnav').fadeIn(2000);
-        backgroundCssTop = parseFloat(instance.$img.css('top'), 10);
-    }
+﻿$(document).ready(function () {
+    //$('#navbar').show();
+
 });
 
-$(document).ready(function () {
-    $('header').height($(window).height());
+$('body').niceScroll({ mousescrollstep: 20, cursorborder: '0px', cursorwidth: '7px', cursoropacitymax: 0.5 });
 
-    $(window).resize(function () {
-        $('header').height($(window).innerHeight);
-    });
-
-    $(window).scroll(function () {
-        $('header').backstretch("pause");
-        var scrollTop = $(window).scrollTop();
-        var coords = backgroundCssTop + (scrollTop / 2) + 'px';
-        $('header img').css({
-            top: coords
-        });
-
-        if (scrollTop >= $('#content').offset().top - ($('#navbar').height())) {
-            $('#navbar').show();
-        } else {
-            $('#navbar').hide();
-        }
-
-    });
-
-    $('body').niceScroll({ mousescrollstep: 20, cursorborder: '0px', cursorwidth: '7px', cursoropacitymax: 0.5 });
-
-    var angle1 = 0;
-    setInterval(function () {
-        angle1 += 0.15;
-        $("#degnirkinnerring").rotate(angle1);
-    }, 150);
-
-    var angle2 = 0;
-    setInterval(function () {
-        angle2 -= 0.3;
-        $("#degnirkouterring").rotate(angle2);
-    }, 70);
-
-    // Scroll on click
-    $(document).on('click', 'header .scrollToHref', function (e) {
-        if (!($('html body').is('.error404'))) {
-            e.preventDefault();
-            var href = e.target.attributes['href'].nodeValue;
-            var distance = $(href).offset().top - $('#navbar').height() + 2;
-            $('html').animate({ scrollTop: distance }, { duration: 1000 });
-        }
-    });
-
-    //https://api.foursquare.com/v2/venues/4d7be2a0f260a093e61f30ba/herenow?client_id=3UFSGCLAXPLGASVZIWDYEF3NL24SDC1RYYS1DMKDTGFJQJ1L&client_secret=XK4WTKZHT2HTUP3OTBCX1G55IST1X2ZQHQNG1GDVYBB2KHWH&v=20130827
-
-
-    //$("#crown").rotate(-15);
-});
 
 (function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {

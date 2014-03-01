5066ce1925166e4771b1198b84b13a4273235fa2
on5sl <bramverswalm@gmail.com>
2014-03-01 14:31:01 +0100

    Revert "Revert "Added the rental partial control""
    This reverts commit 392237db40163501abee7d0323320f84411d58d0.

---
+++ degnirk.be/degnirk.be/Controllers/RentalController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace degnirk.be.Controllers
+{
+    public class RentalController : SurfaceController
+    {
+
+    }
+}

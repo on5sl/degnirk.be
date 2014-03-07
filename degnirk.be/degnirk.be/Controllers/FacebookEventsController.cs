110741bd72b94002878407644ab7b06cd4764212
on5sl <bramverswalm@gmail.com>
2014-03-07 18:20:46 +0100

    Refactored the RenderPartial to RenderAction

---
+++ degnirk.be/degnirk.be/Controllers/FacebookEventsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Mvc;
+using degnirk.be.Models;
+using Service;
+using Umbraco.Web.Mvc;
+
+namespace degnirk.be.Controllers
+{
+    public class FacebookEventsController : SurfaceController
+    {
+        private const int NumberOfEvents = 3;
+        private FacebookService _facebookService;
+        
+        [ChildActionOnly]
+        public PartialViewResult GetFacebookEvents()
+        {
+            var facebookService = new FacebookService(
+                ConfigurationManager.AppSettings["FacebookAppAccessToken"]);
+            var model = new FacebookEventsModel();
+            model.FacebookEvents = facebookService.GetLatestFacebookEvents(
+                ConfigurationManager.AppSettings["FacebookPageId"],
+                NumberOfEvents);
+            return PartialView("FacebookEvents", model);
+        }
+    }
+}
--- degnirk.be/degnirk.be/Models/FacebookEventsModel.cs
+++ degnirk.be/degnirk.be/Models/FacebookEventsModel.cs
@@ -6,16 +6,6 @@ namespace degnirk.be.Models
 
     public class FacebookEventsModel
     {
-        private const int NumberOfEvents = 3;
-        public dynamic FacebookEvents { get; private set; }
-
-        public FacebookEventsModel()
-        {
-            var facebookService = new FacebookService(
-                ConfigurationManager.AppSettings["FacebookAppAccessToken"]);
-            this.FacebookEvents = facebookService.GetLatestFacebookEvents(
-                ConfigurationManager.AppSettings["FacebookPageId"], 
-                NumberOfEvents);
-        }
+        public dynamic FacebookEvents { get; set; }
     }
 }

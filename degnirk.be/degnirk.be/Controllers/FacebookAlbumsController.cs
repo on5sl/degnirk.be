a8b941d11a5be003e14cd89860e1526cc0eb776a
on5sl <bramverswalm@gmail.com>
2014-03-08 12:47:56 +0100

    Forgot to commit the new FacebookAlbumsController

---
+++ degnirk.be/degnirk.be/Controllers/FacebookAlbumsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using degnirk.be.Models;
+using Service;
+using Umbraco.Web.Mvc;
+
+namespace degnirk.be.Controllers
+{
+    public class FacebookAlbumsController : SurfaceController
+    {
+        [ChildActionOnly]
+        public PartialViewResult GetFacebookAlbums(string facebookAppAccessToken, long facebookPageId)
+        {
+            var facebookService = new FacebookService(facebookAppAccessToken);
+            var model = new FacebookAlbumsModel
+            {
+                FacebookAlbums = facebookService.GetFacebookAlbums(facebookPageId)
+            };
+            return PartialView("FacebookAlbums", model);
+        }
+    }
+}

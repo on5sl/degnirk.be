92682be98bef02287469256fee7a43c73bae8494
on5sl <bramverswalm@gmail.com>
2014-03-08 17:38:32 +0100

    Deleted some old stuff

--- degnirk.be/degnirk.be/scripts/DeGnirk.js
+++ degnirk.be/degnirk.be/scripts/DeGnirk.js
@@ -1,6 +1,4 @@
-﻿//$('body').niceScroll({ mousescrollstep: 20, cursorborder: '0px', cursorwidth: '7px', cursoropacitymax: 0.5 });
-
-(function (i, s, o, g, r, a, m) {
+﻿(function (i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
         (i[r].q = i[r].q || []).push(arguments)
     }, i[r].l = 1 * new Date(); a = s.createElement(o), m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
